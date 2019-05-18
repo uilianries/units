384b8b2ee6ff512c4cd601075cab9eca94ba000a
Mateusz Pusz <mateusz.pusz@gmail.com>
2019-05-18 21:22:15 +0200

    safe_multiply added to ratio value

--- src/include/units/ratio.h
+++ src/include/units/ratio.h
@@ -46,8 +46,8 @@ namespace std::experimental::units {
       den(detail::abs(d) / std::gcd(n, d))
     {
       Expects(d != 0); // denominator cannot be zero
-      Expects(n >= -std::numeric_limits<std::intmax_t>::max() &&
-              d >= -std::numeric_limits<std::intmax_t>::max()); // out of range
+      Expects(n >= -INTMAX_MAX); // numerator too negative
+      Expects(d >= -INTMAX_MAX); // numerator too negative
     }
 
     template<std::intmax_t Num, std::intmax_t Den>
@@ -58,9 +58,9 @@ namespace std::experimental::units {
 
     [[nodiscard]] friend consteval ratio operator*(const ratio& lhs, const ratio& rhs)
     {
-      const auto gcd1 = std::gcd(lhs.num, rhs.den);
-      const auto gcd2 = std::gcd(rhs.num, lhs.den);
-      return ratio((lhs.num / gcd1) * (rhs.num / gcd2), (lhs.den / gcd2) * (rhs.den / gcd1));
+      const std::intmax_t gcd1 = std::gcd(lhs.num, rhs.den);
+      const std::intmax_t gcd2 = std::gcd(rhs.num, lhs.den);
+      return ratio(safe_multiply(lhs.num / gcd1, rhs.num / gcd2), safe_multiply(lhs.den / gcd2, rhs.den / gcd1));
     }
 
     [[nodiscard]] friend consteval ratio operator*(std::intmax_t n, const ratio& rhs)
@@ -87,6 +87,30 @@ namespace std::experimental::units {
     [[nodiscard]] friend consteval ratio operator/(const ratio& lhs, std::intmax_t n)
     {
       return lhs / ratio(n);
+    }
+
+  private:
+    // Let c = 2^(half # of bits in an intmax_t)
+    // then we find a1, a0, b1, b0 s.t. N = a1*c + a0, M = b1*c + b0
+    // The multiplication of N and M becomes,
+    // N * M = (a1 * b1)c^2 + (a0 * b1 + b0 * a1)c + a0 * b0
+    // Multiplication is safe if each term and the sum of the terms
+    // is representable by intmax_t.
+    static consteval std::intmax_t safe_multiply(std::intmax_t lhs, std::intmax_t rhs)
+    {
+      constexpr uintmax_t c = uintmax_t(1) << (sizeof(intmax_t) * 4);
+
+      const uintmax_t a0 = detail::abs(lhs) % c;
+      const uintmax_t a1 = detail::abs(lhs) / c;
+      const uintmax_t b0 = detail::abs(rhs) % c;
+      const uintmax_t b1 = detail::abs(rhs) / c;
+
+      Expects(a1 == 0 || b1 == 0); //  overflow in multiplication
+      Expects(a0 * b1 + b0 * a1 < (c >> 1)); // overflow in multiplication
+      Expects(b0 * a0 <= INTMAX_MAX); // overflow in multiplication
+      Expects((a0 * b1 + b0 * a1) * c <= INTMAX_MAX -  b0 * a0); // overflow in multiplication
+
+      return lhs * rhs;
     }
   };
 
--- test/metabench/ratio/ratio_value.h
+++ test/metabench/ratio/ratio_value.h
@@ -40,13 +40,13 @@ namespace units {
     std::intmax_t den;
 
     explicit constexpr ratio(std::intmax_t n, std::intmax_t d = 1):
-      // Note: sign(N) * abs(N) == N
-      num(n * (d < 0 ? -1 : 1) / std::gcd(n, d)),
-      den(detail::abs(d) / std::gcd(n, d))
+    // Note: sign(N) * abs(N) == N
+        num(n * (d < 0 ? -1 : 1) / std::gcd(n, d)),
+        den(detail::abs(d) / std::gcd(n, d))
     {
       Expects(d != 0); // denominator cannot be zero
-      Expects(n >= -std::numeric_limits<std::intmax_t>::max() &&
-              d >= -std::numeric_limits<std::intmax_t>::max()); // out of range
+      Expects(n >= -INTMAX_MAX); // numerator too negative
+      Expects(d >= -INTMAX_MAX); // numerator too negative
     }
 
     // [[nodiscard]] constexpr bool operator==(const ratio &) noexcept = default;
@@ -54,9 +54,9 @@ namespace units {
 
     [[nodiscard]] friend constexpr ratio operator*(const ratio& lhs, const ratio& rhs)
     {
-      const auto gcd1 = std::gcd(lhs.num, rhs.den);
-      const auto gcd2 = std::gcd(rhs.num, lhs.den);
-      return ratio((lhs.num / gcd1) * (rhs.num / gcd2), (lhs.den / gcd2) * (rhs.den / gcd1));
+      const std::intmax_t gcd1 = std::gcd(lhs.num, rhs.den);
+      const std::intmax_t gcd2 = std::gcd(rhs.num, lhs.den);
+      return ratio(safe_multiply(lhs.num / gcd1, rhs.num / gcd2), safe_multiply(lhs.den / gcd2, rhs.den / gcd1));
     }
 
     [[nodiscard]] friend constexpr ratio operator*(std::intmax_t n, const ratio& rhs)
@@ -83,6 +83,30 @@ namespace units {
     [[nodiscard]] friend constexpr ratio operator/(const ratio& lhs, std::intmax_t n)
     {
       return lhs / ratio(n);
+    }
+
+  private:
+    // Let c = 2^(half # of bits in an intmax_t)
+    // then we find a1, a0, b1, b0 s.t. N = a1*c + a0, M = b1*c + b0
+    // The multiplication of N and M becomes,
+    // N * M = (a1 * b1)c^2 + (a0 * b1 + b0 * a1)c + a0 * b0
+    // Multiplication is safe if each term and the sum of the terms
+    // is representable by intmax_t.
+    static constexpr std::intmax_t safe_multiply(std::intmax_t lhs, std::intmax_t rhs)
+    {
+      constexpr std::uintmax_t c = std::uintmax_t(1) << (sizeof(std::intmax_t) * 4);
+
+      const std::uintmax_t a0 = detail::abs(lhs) % c;
+      const std::uintmax_t a1 = detail::abs(lhs) / c;
+      const std::uintmax_t b0 = detail::abs(rhs) % c;
+      const std::uintmax_t b1 = detail::abs(rhs) / c;
+
+      Expects(a1 == 0 || b1 == 0); //  overflow in multiplication
+      Expects(a0 * b1 + b0 * a1 < (c >> 1)); // overflow in multiplication
+      Expects(b0 * a0 <= INTMAX_MAX); // overflow in multiplication
+      Expects((a0 * b1 + b0 * a1) * c <= INTMAX_MAX -  b0 * a0); // overflow in multiplication
+
+      return lhs * rhs;
     }
   };
 
