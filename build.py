e11efe93572e8ad270fca532ab2fe2fc7a03ca0a
Uilian Ries <uilianries@gmail.com>
2019-11-05 11:54:54 -0300

    Update credentials
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- build.py
+++ build.py
@@ -3,9 +3,9 @@ from cpt.packager import ConanMultiPackager
 if __name__ == "__main__":
     builder = ConanMultiPackager(
         # package id
-        username = "mpusz",
+        username = "uilianries",
         channel = "testing",
-        stable_branch_pattern = r"v\d+\.\d+\.\d+.*",
+        stable_branch_pattern = r"master",
 
         # dependencies
         remotes = None,
@@ -16,8 +16,8 @@ if __name__ == "__main__":
         archs = ["x86_64"], #  limit to 64-bit only
 
         # package upload (REMEMBER to set CONAN_PASSWORD environment variable in Travis CI and AppVeyor)
-        login_username = "mpusz",
-        upload = "https://api.bintray.com/conan/mpusz/conan-mpusz"
+        login_username = "uilianries",
+        upload = "https://api.bintray.com/conan/uilianries/conan"
     )
     builder.add_common_builds(pure_c=False)
     for settings, options, env_vars, build_requires, reference in builder.items:
